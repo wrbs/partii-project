680629856b0643e71d30bde34b41fa04f05cb748
Will Robson <wrbs@users.noreply.github.com>
2020-11-30 10:05:52 +0000

    Add latest stock ocaml and use it to run the stock benchmarks

--- benchmarks/sandmark/run_jit_benchmarks.sh
+++ benchmarks/sandmark/run_jit_benchmarks.sh
@@ -3,5 +3,5 @@
 make run_config_bytecode_and_native.json
 make run_config_bytecode.json
 
-BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode_and_native.json make ocaml-versions/4.10.0+stock.bench
+BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode_and_native.json make ocaml-versions/4.11.1+stock.bench
 
