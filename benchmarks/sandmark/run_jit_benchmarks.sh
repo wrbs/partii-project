f8a1072e6043a3322cf28fab643a91e60aeaffbc
Will Robson <wrbs@users.noreply.github.com>
2020-12-16 22:24:04 +0000

    Run the benchmarks

--- benchmarks/sandmark/run_jit_benchmarks.sh
+++ benchmarks/sandmark/run_jit_benchmarks.sh
@@ -6,9 +6,8 @@ make run_config_bytecode_and_native.json
 make run_config_bytecode.json
 
 
-BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit+compileonly.bench
 BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode_and_native.json make ocaml-versions/4.11.1+stock.bench
-
-BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit+disabled.bench
+BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit+compileonly.bench
 BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit.bench
+BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit+disabled.bench
 
