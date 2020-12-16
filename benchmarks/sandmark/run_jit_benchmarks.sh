f9b6d9462a1efdf0a39fe279a9896378968255d3
Will Robson <wrbs@users.noreply.github.com>
2020-12-16 14:57:49 +0000

    Add what should allow running benchmarks with the JIT (but it runs out of memory during compilation)

--- benchmarks/sandmark/run_jit_benchmarks.sh
+++ benchmarks/sandmark/run_jit_benchmarks.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
 make run_config_bytecode_and_native.json
 make run_config_bytecode.json
 
+
+BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit+compileonly.bench
 BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode_and_native.json make ocaml-versions/4.11.1+stock.bench
+
+BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit+disabled.bench
+BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_bytecode.json make ocaml-versions/4.11.1+jit.bench
 
