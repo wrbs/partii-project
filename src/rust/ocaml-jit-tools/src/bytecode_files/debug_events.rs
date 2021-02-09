a3e0f8850e124818aa674c5961e161941bb5eaac
Will Robson <wrbs@users.noreply.github.com>
2021-02-09 14:18:16 +0000

    Make the parser of ML data non-recursive

--- src/rust/ocaml-jit-tools/src/bytecode_files/debug_events.rs
+++ src/rust/ocaml-jit-tools/src/bytecode_files/debug_events.rs
@@ -1,6 +1,5 @@
 use super::ml_data::input_value;
 use super::trailer::{Trailer, DBUG_SECTION};
-use crate::bytecode_files::ml_data::input_list;
 use crate::bytecode_files::MLValue;
 use anyhow::{bail, Context, Result};
 use byteorder::{BigEndian, ReadBytesExt};
@@ -29,7 +28,9 @@ pub fn parse_debug_events(f: &mut File, trailer: &Trailer) -> Result<Option<Debu
 
     for _ in (0..num_eventlists) {
         let orig = section.read_u32::<BigEndian>()?;
-        let entries = input_list(&mut section).context("Problem reading debug event value")?;
+        let list_value = input_value(&mut section).context("Problem reading debug events")?;
+        let entries =
+            parse_event_list(&list_value).context("Problem parsing events from the MLValue")?;
         let absolute_dirs =
             input_value(&mut section).context("Problem reading debug event value")?;
 
@@ -41,4 +42,26 @@ pub fn parse_debug_events(f: &mut File, trailer: &Trailer) -> Result<Option<Debu
     }
 
     Ok(Some(DebugInfo { event_lists }))
+}
+
+fn parse_event_list(list: &MLValue) -> Result<Vec<MLValue>> {
+    let mut events = vec![];
+    let mut current_value = list;
+
+    loop {
+        match &current_value {
+            MLValue::Int(0) => {
+                return Ok(events);
+            }
+            MLValue::Block { tag: 0, items } => match &items[..] {
+                [a, b] => {
+                    events.push(a.clone());
+                    current_value = b;
+                }
+                _ => bail!("Unexpected block in event in list"),
+            },
+
+            _ => bail!("Unexpected value in event list"),
+        }
+    }
 }
