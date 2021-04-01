aee687d157be67a6a8f115b3f55657f0f25122be
Will Robson <wrbs@users.noreply.github.com>
2021-04-01 14:40:12 +0100

    Add NegInt

--- test-programs/src/calls.ml
+++ test-programs/src/calls.ml
@@ -4,9 +4,11 @@ let f3 a0 a1 a2 = ()
 let f4 a0 a1 a2 a3 = ()
 let f5 a0 a1 a2 a3 a4 = ()
 
-let () =
+let run () =
     f1 0;
     f2 0 1;
     f3 0 1 2;
     f4 0 1 2 3;
     f5 0 1 2 3 4
+
+let _ = run ()
