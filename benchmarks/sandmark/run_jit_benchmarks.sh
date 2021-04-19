3d93ed03dc3d9eb762860d5cf78910a2c05339f4
Will Robson <wrbs@users.noreply.github.com>
2021-04-19 09:34:58 +0100

    Get a run of benchmarks on the faster system

--- benchmarks/sandmark/run_jit_benchmarks.sh
+++ benchmarks/sandmark/run_jit_benchmarks.sh
@@ -6,8 +6,7 @@ make run_config_macro_bytecode_and_native.json
 make run_config_macro_bytecode.json
 
 
+# BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_macro_bytecode.json make ocaml-versions/4.11.1+jit+cranelift50.bench
 BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_macro_bytecode.json make ocaml-versions/4.11.1+jit.bench
 BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_macro_bytecode_and_native.json make ocaml-versions/4.11.1+stock.bench
-BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_macro_bytecode.json make ocaml-versions/4.11.1+jit+compileonly.bench
-BUILD_BENCH_TARGET=buildbench_both RUN_CONFIG_JSON=run_config_macro_bytecode.json make ocaml-versions/4.11.1+jit+disabled.bench
 
