c6a57ce79de6fc373ae5f9469edf3eef2bf919e5
Will Robson <wrbs@users.noreply.github.com>
2021-05-24 23:23:40 +0100

    First draft!

--- docs/snippets/add.rs
+++ docs/snippets/add.rs
@@ -1,7 +1,7 @@
 Instruction::ArithInt(ArithOp::Add) => {
     oc_dynasm!(self.ops
-        ; add r_accu, [r_sp]
-        ; dec r_accu
+        ; add r_accu, [r_sp]    ; r_sp is alias for r13
+        ; dec r_accu            ; r_accu is alias for r15
         ; add r_sp, BYTE 8
     );
 }
